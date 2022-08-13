b35f830c0a3ab5487b05fb4234620d6c857fe34d
Cristobal Barberis <cristobal@barberis.com>
2022-08-12 23:27:30 -0400

    fix jest mock

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,11 +1,18 @@
-const Potion = require('../lib/Potion');
+const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion.js');
 
-jest.mock('../lib/Potion');
+jest.mock('../lib/Potion.js');
 
-console.log(new Potion());
+test('creates a player object', () => {
+  const player = new Player('Dave');
 
+  expect(player.name).toBe('Dave');
+  expect(player.health).toEqual(expect.any(Number));
+  expect(player.strength).toEqual(expect.any(Number));
+  expect(player.agility).toEqual(expect.any(Number));
 
-const Player = require('../lib/Player');
+  expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
 
 test("gets player's stats as an object", () => {
   const player = new Player('Dave');
